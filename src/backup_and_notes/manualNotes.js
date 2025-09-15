2b505ef3467487cdcd1413bf7e31bbb6ddc5d4f6
irtep <rasanen.petri@gmail.com>
2025-09-15 15:38:03 +0300

    notes_and_backups

---
+++ src/backup_and_notes/manualNotes.js
@@ -0,0 +1,11 @@
+uranium + kidney + cabbage / arnica Delete Mark as Potion Mark as Close Mark as Nothing
+silver + kidney + crystalline / jaslah Delete Mark as Potion Mark as Close Mark as Nothing
+silver + ear + lettuce / water lily Delete Mark as Potion Mark as Close Mark as Nothing
+nickel + heart + crystalline / jaslah Delete Mark as Potion Mark as Close Mark as Nothing
+nickel + liver + vine seed / lungwort Delete Mark as Potion Mark as Close Mark as Nothing
+starmetal + kidney + potato / mandrake Delete Mark as Potion Mark as Close Mark as Nothing
+osmium + kidney + turnip / mystic carrot Delete Mark as Potion Mark as Close Mark as Nothing
+osmium + lung + lobelia / comfrey Delete Mark as Potion Mark as Close Mark as Nothing
+pyrite + kidney + spinach / h'cliz Delete Mark as Potion Mark as Close Mark as Nothing
+rhodium + kidney + raspberry / bloodroot Delete Mark as Potion Mark as Close Mark as Nothing
+rhodium + lung + cabbage / arnica Delete Mark as Potion Mark as Close Mark as Nothing
